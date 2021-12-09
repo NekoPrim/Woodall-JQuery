dd79f9ca28f41f3fad4f7886f86db06003645769
Desiree Quade <nekodez@gmail.com>
2021-12-09 14:43:17 -0600

    delete button

--- plants.js
+++ plants.js
@@ -2,7 +2,25 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log($('#newPlantButton'));
-    $('#newPlantButton').on('click', onNewPlant);
+    // $('#newPlantButton').on('click', onNewPlant);
+    $(document).on('click', '#newPlantButton', onNewPlant); // this is better than the previous
+    // $('.deletePlantButton').on('click', onDeletePlant); // doesnt work
+
+    //listen for clicks anywhere on the DOM
+    //then check if we're clicking on a .deletePlantButton element
+    //before running 'onDeletePlant()'
+    //
+    //"event delegation"
+    //"using the decendent selector"
+    $(document).on('click', '.deletePlantButton', onDeletePlant);
+}
+
+function onDeletePlant() {
+    // `this` is the DOM element that the event happened to
+    console.log('buh-bye cruel world', this);
+
+    //remove the plant from the DOM
+    $(this).parent().remove();
 }
 
 function onNewPlant() {
@@ -16,10 +34,15 @@ function onNewPlant() {
 
     // and render it in a <li> on the DOM
 
-    $('#plantList').append(`<li>${plantName}</li>`);
+    $('#plantList').append(`<li>
+        ${plantName}
+        <button class="deletePlantButton">
+            Delete
+        </button>
+        </li>`);
 
     // reset the new plant input
 
-    $('#pantNameInput').val('').focus();
+    $('#plantNameInput').val('').focus();
 
 }
