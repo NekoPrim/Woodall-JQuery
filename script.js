64d05bfe982763ec85359e47b00f725aa362e105
Desiree Quade <nekodez@gmail.com>
2021-12-09 11:11:31 -0600

    updated 11:11

--- script.js
+++ script.js
@@ -3,10 +3,14 @@ $(document).ready(onReady);
 function onReady() {
     console.log('in onReady');
     $('.box').first().text('I am a humble box');
+
+
     $('.box').css({
         color: 'white',
         'font-size': '28px'
-    })
+    });
+
+
     $('#plantList').append(`
         <li>   
         Bamboo
@@ -15,12 +19,23 @@ function onReady() {
         </li>`);
     $('body').append(`
         <h1>My awesome new jQuery app</h1>
-        <h5>Welcome, this will be so nice</h5>
+        <h5 id="WelcomeText">Welcome, this will be so nice</h5>
         <p>
         Let me tell you about myself...
         </p>
     `);
-    $('ul').empty()
+
+
+    let newPlant = $('#plantNameInput').val();
+    $('#plantList').append(`<li>${newPlant}</li>`);
+
+    let h5Text = $('#WelcomeText').text();
+    console.log('h5Text is:', h5Text);
+
+    let messUpText = h5Text.split('').reverse().join('');
+    console.log('messedUpText:', messUpText);
+
+    $('#WecomeText').text(messUpText);
 }
 
 let plants = [
